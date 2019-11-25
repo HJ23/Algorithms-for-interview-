a67248cb0e026b8805f468b812d40eac0bd302a5
R0ger_HJ <39130214+HJ23@users.noreply.github.com>
2019-11-26 02:05:56 +0400

    Add files via upload

--- BasicTester/BasicTester.py
+++ BasicTester/BasicTester.py
@@ -1,13 +1,23 @@
+
+# ****************************************************************************
+# Extend this abstract class in order to pass multiple arguments into function
+# Add variables into it
+# ****************************************************************************
+
+class argumentPack():
+    def __init__(self):
+        pass
+
 class BasicTester():
     function=None
     counter=0
     def __init__(self,func=None):
         self.function=func
-    def test(self,query,actualValue):
+    def test(self,packet,actualValue):
         if(self.function==None):
             assert False,"Error : declare function first!"
         else:
-            value=self.function(query)
+            value=self.function(packet)
             if(value==actualValue):
                 self.counter+=1
                 print("Test {} Passed !".format(self.counter))
