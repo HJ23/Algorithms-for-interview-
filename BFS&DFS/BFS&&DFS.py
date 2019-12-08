0123facb425de085560102a856aa00d8d23d6008
R0ger_HJ <39130214+HJ23@users.noreply.github.com>
2019-12-08 23:48:35 +0400

    Update BFS&&DFS.py

--- BFS&DFS/BFS&&DFS.py
+++ BFS&DFS/BFS&&DFS.py
@@ -1,51 +1,62 @@
 # Complexity for BFS O(n)
 # Complexity for DFS O(n)
 
-class node():
-    element=None
-    left=None
-    right=None
-    def __init__(self,element=None):
-        self.element=element
-    def __repr__(self):
-        return str(self.element)
+class Node():
+    def __init__(self,__value__=None):
+        self.left=None
+        self.right=None
+        self.value=__value__
+    def getRight(self):
+        return self.right
+    def getLeft(self):
+        return self.left
+    def setRight(self,node):
+        self.right=node
+    def setLeft(self,node):
+        self.left=node
 
 class BinaryTree():
-    head=None
-    current=None
-    nodes=[]
-    def add(self,element):
-        temp=node(element)
-        if(self.head==None):
-            self.head=temp
-            self.current=self.head
-            self.nodes.append(self.head)
+    def __init__(self):
+        self.queue=[]
+        self.root=None
+    def add(self,value):
+        tempnode=Node(value)
+        if(self.root==None):
+            self.root=tempnode
+            self.queue.append(self.root)
         else:
-            if(self.nodes[0].left==None and self.nodes[0].element>temp.element):
-                self.nodes[0].left=temp
-                self.nodes.append(temp)
-            elif(self.nodes[0].right==None):
-                self.nodes[0].right=temp
-                self.nodes.append(temp)
-            if(self.nodes[0].left!=None and self.nodes[0].right!=None):
-                self.nodes.pop(0)
+            current=self.root
+            previous=self.root
+            while(True):
+                if(current.value<=tempnode.value):
+                    previous=current
+                    current=current.getRight()
+                    if(current==None):
+                        previous.setRight(tempnode)
+                        break
+                elif(current.value>tempnode.value):
+                    previous=current
+                    current=current.getLeft()
+                    if(current==None):
+                        previous.setLeft(tempnode)
+                        break   
                 
     def printTreeBFS(self):
-        queue=[self.head]
+        queue=[self.root]
         while(len(queue)!=0):
             temp=queue.pop(0)
             if(temp!=None):
                 queue.append(temp.left)
                 queue.append(temp.right)
-                print(temp.element)
+                print(temp.value)
     def printTreeDFS(self):
-        stack=[self.head]
+        stack=[self.root]
         while(len(stack)!=0):
             temp=stack.pop(0)
             if(temp!=None):
                 stack.insert(0,temp.right)
                 stack.insert(0,temp.left)
-                print(temp.element)
+                print(temp.value)
                     
 #          34
 #    27         67
